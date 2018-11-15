97e1924123fae28144b5273a943cffe8fdf2adbd
st4s1k <st4s1k24@gmail.com>
2018-11-15 15:33:25 +0200

    User class (update)

--- StreamAPI/src/com/endava/streamapi/User.java
+++ StreamAPI/src/com/endava/streamapi/User.java
@@ -6,8 +6,17 @@ import lombok.Data;
 @Data public class User {
     private final String name;
     private boolean isActive;
-    private String roles;
-    private String emails;
+    private String[] roles;
+    private String[] emails;
     private Double balance;
     private final LocalDateTime registrationDate;
+
+    public User(String name, String[] roles, String[] emails) {
+        this.name = name;
+        isActive = true;
+        this.roles = roles;
+        this.emails = emails;
+        balance = 0d;
+        registrationDate = LocalDateTime.now();
+    }
 }
